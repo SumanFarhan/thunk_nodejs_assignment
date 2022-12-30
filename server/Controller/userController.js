9c11699ca331f9c52e3b39af17fffdef799f8c9a
Suman Farhan <51996425+SumanFarhan@users.noreply.github.com>
2022-12-30 08:22:50 +0500

    update routes

--- server/Controller/userController.js
+++ server/Controller/userController.js
@@ -1,11 +1,17 @@
 const userArr=[]
 
+// exports.signup = (req, res) => {
+//     console.log(req.body);
+//     res.send('SignUp')
+// // let user=req.body.message
+// //         if (check500Error(err,res)) {
+// //             return;
+// //         }else{
+// //             userArr.push(user);
+// //             res.status(200).json({message:'User registered'})
+// //         }
+// }
 exports.signup = (req, res) => {
-let user=req.body.message
-        if (check500Error(err,res)) {
-            return;
-        }else{
-            userArr.push(user);
-            res.status(200).json({message:'User registered'})
-        }
+    console.log(req.body);
+    res.send('SignUp')
 }
--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
+var cors=require('cors')
 const express= require('express')
+const routes=require('./Router/Routes')
 const app =express()
 
 app.use(cors());
 app.use(express.urlencoded({extended:false}))
 app.use(express.json())
 
-app.use('/',require('./Router/Routes.js'))
+app.use('/',routes)
 
 app.listen(3000)
